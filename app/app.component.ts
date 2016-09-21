5fda15c2384bf9d4acb300cd96173a2b0ce2dfc8
vazitest <vazix@msn.com>
2016-09-21 16:25:02 +0800

    组件使用对象

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+
 @Component({
   selector: 'my-app',
-  template: '<h1>Vazi second 中文显示 Angular 2 App</h1>'
+  template: '<h1>{{title}}</h1><h2>{{hero}}详情</h2>'
 })
-export class AppComponent { }
+export class AppComponent {
+  title = '英雄之路';
+  hero = '风之神';
+ }
