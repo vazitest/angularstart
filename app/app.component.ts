91e76b1882949b774db156567de365eacb420ea8
vazitest <vazix@msn.com>
2016-09-21 22:15:37 +0800

    列出英雄

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,11 +5,29 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id: 11, name: '好好先生' },
+  { id: 12, name: '玉皇大帝' },
+  { id: 13, name: '皇母娘娘' },
+  { id: 14, name: '齐天大圣' },
+  { id: 15, name: '托塔天王' },
+  { id: 16, name: '如来佛祖' },
+  { id: 17, name: '天鹏元帅' },
+  { id: 18, name: '菩提道长' },
+  { id: 19, name: '嫦娥姐姐' },
+  { id: 20, name: '华仔大神' }
+];
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
+  <h2>我的大神</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
   <h2>{{hero.name}}详情</h2>
   <div><label>编号:</label>{{hero.id}}</div>
   <div>
@@ -20,6 +38,7 @@ export class Hero {
 })
 export class AppComponent {
   title = '英雄之路';
+  heroes = HEROES;
   hero: Hero = {
     id: 1,
     name: '风之神'
