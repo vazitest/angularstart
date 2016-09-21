39d622c96ab08d52e32fc8d37f4e43797ae69f6f
vazitest <vazix@msn.com>
2016-09-22 00:11:56 +0800

    内联样式示范

--- app/app.component.ts
+++ app/app.component.ts
@@ -22,7 +22,7 @@ const HEROES: Hero[] = [
   selector: 'my-app',
   template: `
   <h1>{{title}}</h1>
-  <h2>我的大神</h2>
+  <h2>大神名单</h2>
     <ul class="heroes">
       <li *ngFor="let hero of heroes">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
@@ -34,7 +34,56 @@ const HEROES: Hero[] = [
     <label>大名:</label>
     <input [(ngModel)] ="hero.name" placeholder="大名">
   </div>
-  `
+  `,
+  styles: [`
+  .selected {
+    background-color: #CFD8DC !important;
+    color: white;
+  }
+  .heroes {
+    margin: 0 0 2em 0;
+    list-style-type: none;
+    padding: 0;
+    width: 15em;
+  }
+  .heroes li {
+    cursor: pointer;
+    position: relative;
+    left: 0;
+    background-color: #EEE;
+    margin: .5em;
+    padding: .3em 0;
+    height: 1.6em;
+    border-radius: 4px;
+  }
+  .heroes li.selected:hover {
+    background-color: #BBD8DC !important;
+    color: white;
+  }
+  .heroes li:hover {
+    color: #607D8B;
+    background-color: #DDD;
+    left: .1em;
+  }
+  .heroes .text {
+    position: relative;
+    top: -3px;
+  }
+  .heroes .badge {
+    display: inline-block;
+    font-size: small;
+    color: white;
+    padding: 0.8em 0.7em 0 0.7em;
+    background-color: #607D8B;
+    line-height: 1em;
+    position: relative;
+    left: -1px;
+    top: -4px;
+    height: 1.8em;
+    margin-right: .8em;
+    border-radius: 4px 0 0 4px;
+  }
+`]
 })
 export class AppComponent {
   title = '英雄之路';
