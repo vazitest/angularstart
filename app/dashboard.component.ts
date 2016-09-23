0934b8194b7058fedb2594813142207a79b68ce3
vazitest <vazix@msn.com>
2016-09-23 19:14:53 +0800

    组件的HTML定制模板与服务注入

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
     selector: "my-dashboard",
-    template: '<h3>我的仪表盘</h3>'
+    templateUrl: "app/dashboard.component.html",
+    //template: '<h3>我的仪表盘</h3>'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+    heroes: Hero[] = [];
 
+    constructor(private heroService:HeroService) {}
+
+    ngOnInit(): void {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(1,5));
+    }
+
+    gotoDetail(hero: Hero): void {
+
+    }
 }
