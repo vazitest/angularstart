00b8f6d307eebadd750876eca514222c0a2482cb
vazitest <vazix@msn.com>
2016-09-24 13:00:34 +0800

    通过Web API添加数据

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -25,6 +25,14 @@ export class HeroService {
         return Promise.reject(error.message || error);
     }
 
+    create(name: string): Promise<Hero> {
+        return this.http
+                .post(this.heroesUrl, JSON.stringify({name:name}), {headers: this.headers})
+                .toPromise()
+                .then(res => res.json().data)
+                .catch(this.handleError);
+    }
+
     update(hero: Hero): Promise<Hero> {
         const url = `${this.heroesUrl}/${hero.id}`;
         return this.http
