cfe4223c7ec22e0040ff3fcd46aa9ec949b2a7b1
vazitest <vazix@msn.com>
2016-10-04 21:45:30 +0800

    开发指南-显示数据

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,25 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 @Component({
   selector: 'my-app',
-  template: '<h1>Vazi second 中文显示 Angular 2 App</h1>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>我最爱的英雄是:{{myHero.name}}</h2>
+    <p>大神:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+       {{hero.name}}
+       </li>
+    </ul>
+    <p *ngIf="heroes.length > 3">有很多大神呢！</p>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    title = '英雄之路';
+    heroes = [ new Hero(1, '华仔大神') ,
+              new Hero(2, '苏神'),
+              new Hero(23, '精神'),
+              new Hero(13 ,'天神')
+              ];
+    myHero = this.heroes[0];
+ }
